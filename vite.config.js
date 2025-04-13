e019ada6f3c7e49949285140460f43edd818ac59
Rakin al Shahriar <rakin.al.shahriar@gmail.com>
2025-04-13 07:36:48 +0000

    shei

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()],
-  base: "/habibatoursreact/"
+    tailwindcss(), react()],
+  build: {
+    outDir: './sheibuild'
+  },
+  base: "/"
 })
